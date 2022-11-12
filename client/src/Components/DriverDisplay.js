413696b7eaa43b06bb7fd555d40ac3da96d6cc36
Bill Zhang <billzhangsc@gmail.com>
2022-11-12 13:55:10 -0800

    Added driver display component

---
+++ client/src/Components/DriverDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function DriverDisplay(props) {
+  // Drivers object would be an object containing a list of drivers
+  const driversObject = props.drivers;
+  return (
+    <div>
+      <h1>Driver Display</h1>
+    </div>
+  );
+}
