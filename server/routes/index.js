88cb36ed99fececd4abe143da569f11e581613b7
Bill Zhang <billzhangsc@gmail.com>
2022-11-12 13:44:20 -0800

    Basic backend works

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
 
+require('dotenv').config();
+const {
+  getInitialState,
+  getDriverState,
+  getPassengerState,
+} = require('./initial');
+const {Hop, ChannelType} = require('@onehop/js');
+const hop = new Hop(process.env.HOP_KEY);
+
+const createChannelId = () => {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'ExpressJs Server for PoolRide'});
+});
+
+router.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello from PoolRide Backend created in ExpressJS!',
+  });
+});
+
+router.get('/id', async (req, res) => {
+  const {id} = await hop.channels.tokens.create();
+  res.json({message: 'Successfully Generated ID!', id: id});
+});
+
+router.get('/createTrip', async (req, res) => {
+  const channelId = createChannelId();
+
+  const tripState = getInitialState(
+    req.get('name'),
+    req.get('destination'),
+    req.get('date'),
+  );
+
+  const channel = await hop.channels.create(
+    ChannelType.UNPROTECTED,
+    `${channelId}`,
+    // Creation Options
+    {
+      // Initial Channel state object
+      state: tripState,
+    },
+  );
+  res.json({message: 'Successfully Generated Lobby!', channelId: channelId});
 });
 
 module.exports = router;
