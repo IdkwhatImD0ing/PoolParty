92faaa8168914a5a0f32c0a33924dd916d45b013
anmallie <96325117+anmallie@users.noreply.github.com>
2022-11-12 19:44:23 -0800

    committing all changes lol

--- client/src/Components/PassengerDisplay.js
+++ client/src/Components/PassengerDisplay.js
@@ -1,9 +1,56 @@
 import React from 'react';
 import {Box, Typography, Stack, Button, TextField} from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+
+import AddIcon from '@mui/icons-material/Add';
+import ListSubheader from '@mui/material/ListSubheader';
+
+const makeTestPassengers = () => {
+  return ({
+    'Patrick Passenger': {name: 'Patrick', contact: 'patty@ucsc.edu'},
+    'Nessie': {name: 'Loch Nessie', contact: 'lochland@gmail.com'},
+    'Alan Watts': {name: 'Alan Watts', contact: '424-988-0992'},
+  });
+};
 
 export default function PassengerDisplay(props) {
   // Dictionary  of passenger objects
-  const passengers = props.passengers;
+  // const passengers = props.passengers;
+  const passengers = makeTestPassengers();
 
-  return <></>;
+  /*
+  passenger object
+{
+name:
+contact:
+}*/
+
+  return (
+    <List dense
+      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', borderRadius: '16px' }}
+      subheader={
+        <ListSubheader component="div" id="nested-list-subheader" sx={{borderRadius: '16px'}}>
+          <Typography variant='h6'>
+            Unseated Passengers
+          </Typography>
+        </ListSubheader>
+      }>
+      {Object.entries(passengers).map(([name, passenger]) => {
+        return (
+          <ListItem
+            key={name}
+            secondaryAction={
+              <IconButton>
+                <AddIcon/>
+              </IconButton>
+            }>
+              <ListItemText primary={`${passenger['name']}`} edge='start'/>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
 }
