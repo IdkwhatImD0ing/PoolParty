410890dd6530c8b679a6fcf4f6b3ecd5388fac16
Bill Zhang <billzhangsc@gmail.com>
2022-11-13 03:17:18 -0800

    Fixed join

--- client/src/Components/JoinPassenger.js
+++ client/src/Components/JoinPassenger.js
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
-import { Typography, Stack, Button, TextField } from "@mui/material";
+import React, {useState} from 'react';
+import {Typography, Stack, Button, TextField} from '@mui/material';
 
 export default function JoinPassenger(props) {
-  const [passengerName, setPassengerName] = useState("");
-  const [passengerContact, setPassengerContact] = useState("");
+  const [passengerName, setPassengerName] = useState('');
+  const [passengerContact, setPassengerContact] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (passengerName === "") {
-      alert("Please fill out name");
+    if (passengerName === '') {
+      alert('Please fill out name');
       return;
     }
 
-    fetch("https://poolserver.hop.sh/joinPassenger", {
+    fetch('https://poolserver.hop.sh/joinPassenger', {
       headers: {
         channelId: props.tripId,
         name: passengerName,
@@ -28,15 +28,16 @@ export default function JoinPassenger(props) {
       position="absolute"
       top="50%"
       left="50%"
+      width={window.innerWidth < 600 ? '80%' : '30%'}
       direction="column"
       alignItems="center"
       justifyContent="center"
       spacing={2}
       sx={{
-        backgroundColor: "white",
-        transform: "translate(-50%, -50%)",
-        padding: "2%",
-        borderRadius: "16px",
+        backgroundColor: 'white',
+        transform: 'translate(-50%, -50%)',
+        padding: '2%',
+        borderRadius: '16px',
       }}
     >
       <Typography variant="h3">Add Passenger</Typography>
@@ -54,7 +55,9 @@ export default function JoinPassenger(props) {
         value={passengerContact}
         onChange={(e) => setPassengerContact(e.target.value)}
       />
-      <Button onClick={handleSubmit} variant="contained">Add</Button>
+      <Button onClick={handleSubmit} variant="contained">
+        Add
+      </Button>
     </Stack>
   );
 }
