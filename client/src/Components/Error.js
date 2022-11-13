ced74ecfb43b36566621a866c8b08cd6b346a270
Bill Zhang <billzhangsc@gmail.com>
2022-11-12 17:32:44 -0800

    Added redirect

--- client/src/Components/Error.js
+++ client/src/Components/Error.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Box, Typography, Stack, Button, TextField} from '@mui/material';
+import {useNavigate} from 'react-router-dom';
 
 export default function Error() {
+  const navigate = useNavigate();
   return (
     <Box
       height="100vh"
@@ -14,27 +16,33 @@ export default function Error() {
         backgroundColor: 'lightblue',
       }}
     >
-      <Typography variant="h1" color="black">404</Typography>
-      <Typography variant="h2" color="black" maxWidth={"90%"}>Error: trip not found</Typography>
-      <Typography variant="h5" color="#32b2b7">make sure you copied the URL correctly!</Typography>
-      <Button 
-          onClick={() => {
-            alert('clicked');
-            }}
-            sx={{
-              backgroundColor: '#d2c457',
-              color: 'black',
-              fontFamily: 'sans-serif',
-              fontSize: '1.5rem',
-              marginTop: '3%',
-              marginBottom: '5%',
-              marginLeft: '8%',
-              marginRight: '8%',
-              padding: '1%',
-            }}
-            >
-          Back to Home
-        </Button>
+      <Typography variant="h1" color="black">
+        404
+      </Typography>
+      <Typography variant="h2" color="black" maxWidth={'90%'}>
+        Error: trip not found
+      </Typography>
+      <Typography variant="h5" color="#32b2b7">
+        make sure you copied the URL correctly!
+      </Typography>
+      <Button
+        onClick={() => {
+          navigate('/');
+        }}
+        sx={{
+          backgroundColor: '#d2c457',
+          color: 'black',
+          fontFamily: 'sans-serif',
+          fontSize: '1.5rem',
+          marginTop: '3%',
+          marginBottom: '5%',
+          marginLeft: '8%',
+          marginRight: '8%',
+          padding: '1%',
+        }}
+      >
+        Back to Home
+      </Button>
     </Box>
   );
 }
